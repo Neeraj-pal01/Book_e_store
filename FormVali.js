d5e03b432ca82525005ecbd772fbf17db366dc3a
Neeraj pal <ansupal01@gmail.com>
2025-01-21 12:10:18 +0530

    make it responsive

--- FormVali.js
+++ FormVali.js
@@ -1,44 +1,11 @@
-function formValidate() {
-    var name = document.myform.fullName.value;
-    var email = document.myform.email.value;
-    var Age = document.myform.age.value;
-    var pass = document.myform.password.value;
-    var gender= document.myform.gender.value;
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-    if (name == null || name == '') {
-        document.getElementById('changeName').style.display = "block"
-        document.getElementById("fullName").style.border = "2px solid red"
-        return false;
-    }
-    else if (pass == null || pass == '' || pass.length <= 6) {
-        document.getElementById('changePass').innerText = "Minimum 6 character required";
-        document.getElementById('changePass').style.display = "block"
-        document.getElementById("password").style.border = "2px solid red"
-        return false;
-    }
-    else if (Age == null || Age == '') {
-        document.getElementById('changeAge').style.display = "block"
-        document.getElementById("age").style.border = "2px solid red"
-        return false
-    }
-    else if (pass.indexOf('@')==0) {
-        document.getElementById('changePass').innerText = "@ is Mandatory";
-        document.getElementById('changePass').style.display = "block"
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
 
-        return false      
-    }
-    else if (gender=='') {
-        document.getElementById('changePass').innerText = "@ is Mandatory";
-        return false      
-    }
-    else if (email.indexOf("@") == 0) {
-        document.getElementById('changeMail').innerText = "Enter a valid Mail which includes @";
-        document.getElementById("email").style.border = "2px solid red"
-        return false;
-    }
-    else {
-        alert("register succesfully");
-             open("anime.html");
-
-    }
-}
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
